3ee31545079bbd0ed71309d052d43815a29c6875
Esther Kang <eskang87@gmail.com>
2021-12-02 16:42:41 -0800

    commit tree 9aa91decd8f41cdb6b803e65cb43183e22c188ed

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+/* function superbowlWin(record) {
+    record.find(obj => {
+        if(obj.result === "W"){
+            return obj.year
+        } else if (obj.result === "N/A"){
+            return obj.year
+        }
+    })
+} */
+
+/*function superbowlWin(record) {
+    const win = record.find(obj => {
+         if(obj.result === "W"){
+             return obj.year
+         }  
+     })
+    return win;
+ };*/
+
+
+ /*function superbowlWin(record) {
+   const win = record.find(obj => {
+        if(obj.result === "W"){
+            return obj
+        } else {
+            return undefined
+        }
+    })
+   return win.year;
+}; */
+
+function superbowlWin(record) {
+    const win = record.find(obj => obj.result === "W")
+    return (win ? win.year : undefined)
+ }; 
+
+
+
+/* function winningYear(obj) {
+    if (obj.result === 'W'){
+        return obj.year
+    } 
+} */
 
